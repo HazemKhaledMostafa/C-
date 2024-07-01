22d7fd94928b5d904beeef75a68560664dd3e7d3
HazemKhaledMostafa <hazemsaid001@outlook.com>
2024-07-02 02:36:29 +0300

    Add files via upload

---
+++ Hackerrank/Conditional Statements.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>> n;
+    if(n >= 1 && n <= 9)
+    {
+        switch(n){
+            case 1: cout<<"one";break;
+            case 2: cout<<"two";break;
+            case 3: cout<<"three";break;
+            case 4: cout<<"four";break;
+            case 5: cout<<"five";break;
+            case 6: cout<<"six";break;
+            case 7: cout<<"siven";break;
+            case 8: cout<<"egiht";break;
+            case 9: cout<<"nine";break;
+        }
+    }
+    else
+    {
+        cout<<"Greater than 9";
+    }
+}
